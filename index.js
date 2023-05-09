a370aeb679f5f81b67c3b26410e5211e4d777590
Gauthier Painteaux <gauthier.painteaux@globalis-ms.com>
2023-05-09 17:05:49 +0200

    WIP : Chat -> Add usersList

--- index.js
+++ index.js
@@ -6,17 +6,11 @@ const crypto = require('crypto');
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+var users = {};
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
-
-function sendUserJoin(socket, channel, id, username) {
-    socket.to(channel).emit('userJoin', { 'id' : id, 'username' : username })
-}
-
-function sendUserLeft(socket, channel, id, username) {
-    socket.to(channel).emit('userLeft', { 'id' : id, 'username' : username });
-}
 
 io.on('connection', (socket) => {
     console.log('Client connected : ' + socket.id);
@@ -25,23 +19,51 @@ io.on('connection', (socket) => {
     var channel = '';
     var typingTO = false;
 
+    var sendUserJoin = (channel, id, username) => {
+        socket.to(channel).emit('userJoin', { 'id' : id, 'username' : username });
+    }
+
+    var sendUserLeft = (channel, id, username) => {
+        socket.to(channel).emit('userLeft', { 'id' : id, 'username' : username });
+    }
+
+    var updateUsersList = (channel, id, username, isRemoving = false) => {
+        if (!channel || !id) {
+            return;
+        }
+
+        if (isRemoving) {
+            delete users[channel][id];
+        } else {
+            if (users[channel] === undefined) {
+                users[channel] = {};
+            }
+            users[channel][id] = username;
+        }
+
+        io.sockets.in(channel).emit('updateUsers', { 'users' : users[channel] });
+    }
+
     socket.on('joinChannel', (data) => {
         username = data.username;
         channel = data.channel;
         console.log(username + ' joined channel ' + channel);
         socket.join(channel);
-        sendUserJoin(socket, channel, id, username);
+        sendUserJoin(channel, id, username);
+        updateUsersList(channel, id, username);
     });
 
     socket.on('changeChannel', (newChannel) => {
         console.log(username + ' switched to channel ' + newChannel);
-        sendUserLeft(socket, channel, id, username);
+        sendUserLeft(channel, id, username);
+        updateUsersList(channel, id, username, true);
         socket.leave(channel);
 
         channel = newChannel;
 
         socket.join(channel);
-        sendUserJoin(socket, channel, id, username);
+        sendUserJoin(channel, id, username);
+        updateUsersList(channel, id, username);
     });
 
     socket.on('sendMessage', (message) => {
@@ -66,7 +88,8 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         console.log('Client disconnected : ' + socket.id);
-        sendUserLeft(socket, channel, id, username);
+        sendUserLeft(channel, id, username);
+        updateUsersList(channel, id, username, true);
     });
 });
 
