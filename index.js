46137ea8010405abd4dbed324929c63084f8d549
Gauthier Painteaux <gauthier.painteaux@globalis-ms.com>
2023-05-10 17:19:10 +0200

    Add some interresting features

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 
 var users = {};
+var messages = {};
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -17,13 +18,17 @@ io.on('connection', (socket) => {
     var id = crypto.randomUUID();
     var username = '';
     var channel = '';
+    var lastMessage = '';
     var typingTO = false;
+    var wizzTO = false;
 
     var sendUserJoin = (channel, id, username) => {
+        updateMessagesList(channel, username + ' joined the game');
         socket.to(channel).emit('userJoin', { 'id' : id, 'username' : username });
     }
 
     var sendUserLeft = (channel, id, username) => {
+        updateMessagesList(channel, username + ' left the game');
         socket.to(channel).emit('userLeft', { 'id' : id, 'username' : username });
     }
 
@@ -44,12 +49,50 @@ io.on('connection', (socket) => {
         io.sockets.in(channel).emit('updateUsers', { 'users' : users[channel] });
     }
 
+    var updateMessagesList = (channel, message) => {
+        if (!channel || !message) {
+            return;
+        }
+
+        if (messages[channel] === undefined) {
+            messages[channel] = [];
+        }
+
+        messages[channel].push(message);
+    }
+
+    var sendMessagesHistory = (channel) => {
+        console.log('History of channel ' + channel + ' sent to ' + username);
+        io.to(socket.id).emit('messagesHistory', { 'messages' : messages[channel] });
+    }
+
     socket.on('joinChannel', (data) => {
         username = data.username;
         channel = data.channel;
+        usernameAlreadyExist = false;
+
+        for (const channel in users) {
+            for (const userID in users[channel]) {
+                if (users[channel][userID] !== username) {
+                    continue;
+                }
+
+                usernameAlreadyExist = true;
+                break;
+            }
+        }
+
+        if (usernameAlreadyExist) {
+            console.log(username + ' already exist');
+            io.to(socket.id).emit('usernameAlreadyExist');
+            return;
+        }
+
         console.log(username + ' joined channel ' + channel);
         socket.join(channel);
+        io.to(socket.id).emit('connected');
         sendUserJoin(channel, id, username);
+        sendMessagesHistory(channel);
         updateUsersList(channel, id, username);
     });
 
@@ -63,12 +106,19 @@ io.on('connection', (socket) => {
 
         socket.join(channel);
         sendUserJoin(channel, id, username);
+        sendMessagesHistory(channel);
         updateUsersList(channel, id, username);
     });
+
+    socket.on('getLastMessage', () => {
+        io.to(socket.id).emit('lastMessageSent', lastMessage);
+    })
 
     socket.on('sendMessage', (message) => {
         console.log('Message : ' + message);
         socket.to(channel).emit('receiveMessage', username + ' : ' + message);
+        updateMessagesList(channel, username + ' : ' + message);
+        lastMessage = message;
         typingTO = false;
     });
 
@@ -83,6 +133,18 @@ io.on('connection', (socket) => {
 
         setTimeout(function () {
             typingTO = false;
+        }, 4000);
+    });
+
+    socket.on('sendWizz', () => {
+        if (wizzTO) {
+            return;
+        }
+
+        socket.to(channel).emit('wizz');
+
+        setTimeout(function () {
+            wizzTO = false;
         }, 4000);
     });
 
